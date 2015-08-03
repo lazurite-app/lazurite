cf74bb548f764064ab85df32720932b4dcba2b25
Hugh Kennedy <hughskennedy@gmail.com>
2015-08-04 01:34:54 +0200

    testing events and such

--- app-main/index.js
+++ app-main/index.js
@@ -1,2 +1,6 @@
 document.registerElement('app-sidebar', require('app-sidebar'))
 document.registerElement('app-scene-select', require('app-scene-select'))
+
+document.body.addEventListener('app-scene-select', e => {
+  console.log('it works!', e.data)
+}, true)
--- app-scene-select/index.js
+++ app-scene-select/index.js
@@ -1,3 +1,5 @@
+import Event from 'synthetic-dom-events'
+import findup from 'findup-element'
 import domify from 'domify'
 
 const fs = require('fs')
@@ -6,5 +8,35 @@ const html = fs.readFileSync(__dirname + '/index.html', 'utf8')
 export default class AppSceneSelect extends HTMLElement {
   createdCallback () {
     this.appendChild(domify(html))
+    this.list = this.createList()
+    this.querySelector('ul').appendChild(this.list)
+
+    this.addEventListener('click', e => {
+      const el = findup(e.target, (
+        el => el.hasAttribute && el.hasAttribute('data-scene')
+      ))
+
+      if (!el) return
+
+      this.dispatchEvent(Event('app-scene-select', {
+        data: 'placeholder'
+      }))
+    }, false)
+  }
+
+  attachedCallback () {
+  }
+
+  // good place for vdom I think
+  createList () {
+    const list = document.createDocumentFragment()
+
+    for (var i = 0; i < 24; i++) {
+      const li = document.createElement('li')
+      li.setAttribute('data-scene', i)
+      list.appendChild(li)
+    }
+
+    return list
   }
 }
