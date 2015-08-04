b55a4f339165ba8c59a5f6f38dac1ce107af8fc4
Yoshua Wuyts <i@yoshuawuyts.com>
2015-08-04 11:47:32 +1000

    app-styles: fix reset

--- app-scene-select/index.js
+++ app-scene-select/index.js
@@ -5,7 +5,7 @@ import domify from 'domify'
 const fs = require('fs')
 const html = fs.readFileSync(__dirname + '/index.html', 'utf8')
 
-export default class AppSceneSelect extends HTMLElement {
+export default class AppSceneSelect extends window.HTMLElement {
   createdCallback () {
     this.appendChild(domify(html))
     this.list = this.createList()
