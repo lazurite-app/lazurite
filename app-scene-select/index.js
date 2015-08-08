b5b9bc96bf5a98bd4a5588bf59a7ecc63a8507a9
Hugh Kennedy <hughskennedy@gmail.com>
2015-08-08 10:25:05 -0700

    attempting to get mouse clicks working

--- app-scene-select/index.js
+++ app-scene-select/index.js
@@ -12,35 +12,35 @@ export default class AppSceneSelect extends window.HTMLElement {
     // render dom nodes
     // null -> null
     function render (h) {
-      return h('ul.cf', createList(h, dispatch))
+      return h('ul.cf', createList(h))
     }
 
     // dispatch an `app-scene-select` event
     // num -> null -> null
-    function dispatch (i) {
-      return () => {
+    function dispatch (scene) {
+      return e => {
+        console.log('dispatch?')
         self.dispatchEvent(Event('app-scene-select', {
-          data: 'placeholder'
+          scene: scene
         }))
       }
+    }
+
+    // create a list
+    // fn -> HTMLElement
+    function createList (h) {
+      const list = []
+
+      for (var i = 0; i < scenes.length; i++) {
+        list.push(h('li', {
+          'ev-click': dispatch(scenes[i])
+        }, h('label', scenes[i])))
+      }
+
+      return list
     }
   }
 
   attachedCallback () {
   }
-}
-
-// create a list
-// fn -> HTMLElement
-function createList (h, dispatch) {
-  const list = []
-
-  for (var i = 0; i < scenes.length; i++) {
-    list.push(h('li', {
-      'ev-click': dispatch(i),
-      'data-scene': scenes[i]
-    }, h('label', scenes[i])))
-  }
-
-  return list
 }
