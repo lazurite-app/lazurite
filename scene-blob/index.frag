233954c2262ab6df8d6e08fe5b17435cc427f7b8
Hugh Kennedy <hughskennedy@gmail.com>
2015-08-08 09:59:57 -0700

    working time, etc.

--- scene-blob/index.frag
+++ scene-blob/index.frag
@@ -4,6 +4,6 @@ uniform float iGlobalTime;
 uniform float speed;
 
 void main() {
-  float r = sin(iGlobalTime * speed) * .5 + .5;
+  float r = sin(iGlobalTime) * .5 + .5;
   gl_FragColor = vec4(r, 0, 1, 1);
 }
--- scene-renderer/index.js
+++ scene-renderer/index.js
@@ -5,8 +5,11 @@ const Emitter = require('events/')
 const after = require('after')
 const Path = require('path')
 const raf = require('raf')
+const fs = require('fs')
 
 module.exports = SceneRenderer
+
+const start = Date.now()
 
 inherits(SceneRenderer, Emitter)
 function SceneRenderer (gl) {
@@ -20,10 +23,11 @@ function SceneRenderer (gl) {
   this.tick()
 }
 
+SceneRenderer.scenes = require('./package.json').scenes
 SceneRenderer.prototype.tick = function tick () {
   raf(this.tick)
   if (!this.current) return
-  this.current.emit('step')
+  this.current.emit('step', (Date.now() - start) / 1000)
 }
 
 SceneRenderer.prototype.use = function use (scene) {
@@ -31,7 +35,7 @@ SceneRenderer.prototype.use = function use (scene) {
     this.current.disable()
   }
 
-  this.current = scene
+  this.current = SceneWrapper(gl, scene)
   this.current.enable()
 
   return this
@@ -39,11 +43,11 @@ SceneRenderer.prototype.use = function use (scene) {
 
 const sceneCache = {}
 
-inherits(Scene, Emitter)
-function Scene (gl, name) {
+inherits(SceneWrapper, Emitter)
+function SceneWrapper (gl, name) {
   if (sceneCache[name]) return sceneCache[name]
-  if (!(this instanceof Scene)) {
-    return new Scene(gl, name)
+  if (!(this instanceof SceneWrapper)) {
+    return new SceneWrapper(gl, name)
   }
 
   Emitter.call(this)
@@ -61,7 +65,6 @@ function Scene (gl, name) {
   const labels = Object.keys(shaders)
   const next = after(labels.length, done)
   const self = this
-  const start = Date.now()
 
   self.enabled = false
   self.ready = false
@@ -108,19 +111,25 @@ function Scene (gl, name) {
   })
 }
 
-Scene.prototype.enable = function () {
+Object.defineProperty(SceneWrapper.prototype, 'time', {
+  get: function () {
+    return (Date.now() - start) / 1000
+  }
+})
+
+SceneWrapper.prototype.enable = function () {
   if (this.enabled) return
   if (!this.ready) {
     return this.once('ready', this.enable.bind(this))
   }
 
-  this.emit('init')
+  this.emit('init', (Date.now() - start) / 1000)
   this.enabled = true
 }
 
-Scene.prototype.disable = function () {
+SceneWrapper.prototype.disable = function () {
   if (!this.enabled) return
-  this.emit('stop')
+  this.emit('stop', (Date.now() - start) / 1000)
   this.enabled = false
 }
 
@@ -134,4 +143,4 @@ canvas.style.left = 0
 canvas.style.top = 0
 
 SceneRenderer(gl)
-  .use(Scene(gl, 'scene-blob'))
+  .use('scene-blob')
