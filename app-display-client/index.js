c9b7a3cb6e810c9a3d931afe2a284544122756ec
Hugh Kennedy <hughskennedy@gmail.com>
2015-08-30 14:34:40 -0600

    IT LIVES

--- app-display-client/index.js
+++ app-display-client/index.js
@@ -1,6 +1,7 @@
-import Event from 'synthetic-dom-events'
+import Renderer from 'scene-renderer'
 import querystring from 'querystring'
 import signalhub from 'signalhub'
+import Fit from 'canvas-fit'
 
 const hubPort = querystring.parse(String(
   window.location.search
@@ -8,19 +9,39 @@ const hubPort = querystring.parse(String(
 
 export default class AppDisplayClient extends window.HTMLElement {
   createdCallback () {
-    this.client = signalhub('lazurite-client', [
+    const canvas = this.appendChild(document.createElement('canvas'))
+    const gl = canvas.getContext('webgl')
+    const client = signalhub('lazurite-client', [
       `http://localhost:${hubPort}`
     ])
 
-    this.client.subscribe('updates').on('data', data => {
-      console.log('update', data)
-      this.dispatchEvent(Event('lazurite-client-data', {
-        data: data
-      }))
+    const values = [{}, {}]
+    const renderers = ['left', 'right'].map((side, i) => {
+      return new Renderer(gl, {
+        left: side === 'left',
+        right: side === 'right',
+        values: values[i],
+        manual: false
+      }).use('scene-warp')
     })
 
-    this.client.subscribe('reset').on('data', e => {
-      console.log('reset', e)
+    client.subscribe('updates').on('data', data => {
+      const sidx = data.side === 'left' ? 0 : 1
+      values[sidx][data.key] = data.value
     })
+
+    client.subscribe('reset').on('data', data => {
+      const sidx = data.side === 'left' ? 0 : 1
+      const vals = values[sidx]
+      const repl = data.data
+
+      renderers[sidx].use(data.scene).once('change', function () {
+        Object.keys(repl).forEach(key => {
+          vals[key] = repl[key]
+        })
+      })
+    })
+
+    window.addEventListener('resize', Fit(canvas), false)
   }
 }
